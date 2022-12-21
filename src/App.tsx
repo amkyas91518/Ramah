035acaf040f653f99bf4e35ff7514506981f065c
Ahmed El Shazly <amkyas91518@gmail.com>
2022-12-21 10:40:50 +0200

    ad React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import AllOffers from "./components/AllOffers";
 import LandingPage from "./components/LandingPage";
 import NavBar from "./components/NavBar";
 import UserOrders from "./components/UserOrders";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
