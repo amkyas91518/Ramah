26c09cd26c34581be1397a5fb92cd4494af4a15f
Ahmed El Shazly <amkyas91518@gmail.com>
2022-12-21 10:26:07 +0200

    setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
